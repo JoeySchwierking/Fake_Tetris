a75138258a39d0463b50ed4e058acbe12c92b215
JoeySchwierking <jschwierking1@gmail.com>
2018-07-25 15:02:09 -0500

    MORE COLORS

--- very_real_tetris.py
+++ very_real_tetris.py
@@ -13,6 +13,8 @@ class stack():
 		self.gaming = True
 		self.stop = False
 		self.t = 0
+		self.u = 7
+		self.v = 1.0
 #		sense.set_pixel(self.t, 7, (192, 74, 35))
 #		time.sleep(.4)
 #		sense.clear()
@@ -24,15 +26,22 @@ class stack():
 		pygame.time.set_timer(USEREVENT +1, 800)
 		while self.gaming:
 			for event in pygame.event.get():
-				sense.set_pixel(self.t, 7, (192, 74, 35))
-				time.sleep(.4)
-				sense.clear()
+				self.stop = False
+				sense.set_pixel(self.t, self.u, (255, 255, 255))
+				time.sleep(v)
+				if event.type == KEYDOWN:
+					self.u -= 1
+					self.stop = True
+					if self.u < 0:
+						sense.set_pixel(self.t,self.u,(192, 74, 35))
+						self.gaming = False
+						self.v -=.1
+#				time.sleep(0.4)
 				if self.stop == False:
+					sense.set_pixel(self.t,self.u, (0,0,0))
 					self.t += 1
 					if self.t > 7:
 						self.t = 0
-				if event.type == KEYDOWN:
-					self.stop = True
 
 
 if __name__ == '__main__':
